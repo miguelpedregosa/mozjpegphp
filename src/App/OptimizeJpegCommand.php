916fe67146bfcdc986727e0ff4a67c381350df1e
migue <miguelpedregosa@gmail-com>
2015-02-25 22:33:32 +0100

    Cambio de funcionamiento, se otimizan las imagenes a una carpeta de destino y se permite optimizar un directorio completo

--- src/App/OptimizeJpegCommand.php
+++ src/App/OptimizeJpegCommand.php
@@ -20,6 +20,7 @@ class OptimizeJpegCommand extends Command
     private $moz_jpeg = '/opt/mozjpeg/bin/jpegtran';
     private $cjpeg = '/opt/mozjpeg/bin/cjpeg';
 
+    private $optimized_images = array();
 
     protected function configure()
     {
@@ -27,7 +28,7 @@ class OptimizeJpegCommand extends Command
             ->setDescription('Optimiza un archivo jpeg usando la librería MozJpeg')
             ->addArgument(
                 'source',
-                InputArgument::REQUIRED,
+                InputArgument::OPTIONAL,
                 'Fichero de origen a optimizar'
             )
             ->addArgument(
@@ -41,6 +42,13 @@ class OptimizeJpegCommand extends Command
                 InputOption::VALUE_OPTIONAL,
                 'Nivel de calidad para recodificar el fichero',
                 null
+            )
+            ->addOption(
+                'd',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Directorio de destino para las imágenes optimizadas',
+                null
             );
     }
 
@@ -48,42 +56,119 @@ class OptimizeJpegCommand extends Command
     {
         try {
             $source = $input->getArgument('source');
-            $dest = $input->getArgument('dest');
-
-            if (!is_file($source)) {
-                {
-                    throw new \Exception('Invalid source file');
-                }
-            }
-            $source_size = filesize($source);
-            if (!$dest) {
-                $pathinfo = pathinfo($source);
-                $dest = $pathinfo['dirname'] . '/' . $pathinfo['filename'] . '_moz.' . $pathinfo['extension'];
-            }
             $quality = $input->getOption('q');
-            if (is_numeric($quality)) {
-                exec(sprintf('%s -quality %d -outfile /tmp/mozjpeg_tmp.jpg %s', $this->cjpeg, $quality, $source));
-            } else {
-                copy($source, '/tmp/mozjpeg_tmp.jpg');
+            $dest = $this->getDestinationDirectory($input->getOption('d'), $quality);
+
+            //Origen (si no se pasa, usa el directorio actual)
+            if (!$source) {
+                $source = getcwd();
             }
-            if (!is_file('/tmp/mozjpeg_tmp.jpg')) {
-                {
-                    throw new \Exception('Invalid file');
+
+            //Directorio de destino
+            if (!$dest) {
+                throw new \Exception('Invalid destination directory');
+            }
+            //El origen puede ser un fichero o un directorio con imagenes
+            if (is_file($source)) {
+                $this->optimizeFile($output, $source, $dest, $quality);
+            } elseif (is_dir($source)) {
+                if ($handle = opendir($source)) {
+                    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                    while (false !== ($entry = readdir($handle))) {
+                        if ($entry != "." && $entry != ".." && !is_dir($entry)) {
+                            if (strpos(finfo_file($finfo, $entry), 'image') !== false) {
+                                $this->optimizeFile($output, $entry, $dest, $quality);
+                            }
+                        }
+                    }
+                    closedir($handle);
                 }
             }
-            exec(sprintf("%s -copy none %s > %s", $this->moz_jpeg, '/tmp/mozjpeg_tmp.jpg', $dest));
-            unlink('/tmp/mozjpeg_tmp.jpg');
-            $dest_size = filesize($dest);
-            $output->writeln(
-                sprintf(
-                    'Optimización completada: %s --> %s',
-                    $this->humanFilesize($source_size),
-                    $this->humanFilesize($dest_size)
-                )
-            );
+            if (count($this->optimized_images)) {
+                $gain = 0;
+                array_walk(
+                    $this->optimized_images,
+                    function ($elem) use (&$gain) {
+                        $gain += $elem;
+                    }
+                );
+                $output->writeln(
+                    sprintf(
+                        "Se han optimizado %d archivos%s con una ganancia de %s",
+                        count($this->optimized_images),
+                        is_numeric($quality) ? '(calidad ' . (int)$quality . '%)' : '',
+                        $this->humanFilesize($gain)
+                    )
+                );
+            }
+
         } catch (\Exception $e) {
             $output->writeln('<error>' . $e->getMessage() . '</error>');
         }
+    }
+
+    /**
+     * @param null $dest
+     * @param null $quality
+     * @return bool|null|string
+     */
+    private function getDestinationDirectory($dest = null, $quality = null)
+    {
+        //Directorio por defecto
+        if (is_null($dest)) {
+            $dest = (is_numeric($quality)) ? sprintf('optimized_%d', $quality) : 'optimized';
+        }
+
+        if (is_dir($dest) && is_writable($dest)) {
+            return $dest;
+        }
+        if (mkdir($dest)) {
+            return $dest;
+        }
+        return false;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param $source
+     * @param null $dest
+     * @param null $quality
+     * @throws \Exception
+     */
+    private function optimizeFile(OutputInterface $output, $source, $dest = null, $quality = null)
+    {
+        if (!is_file($source)) {
+            {
+                throw new \Exception('Invalid source file');
+            }
+        }
+        $source_size = filesize($source);
+        $pathinfo = pathinfo($source);
+        $dest_file = $dest . '/' . $pathinfo['filename'] . $pathinfo['extension'];
+
+        if (is_numeric($quality)) {
+            exec(sprintf('%s -quality %d -outfile /tmp/mozjpeg_tmp.jpg %s', $this->cjpeg, $quality, $source));
+        } else {
+            copy($source, '/tmp/mozjpeg_tmp.jpg');
+        }
+        if (!is_file('/tmp/mozjpeg_tmp.jpg')) {
+            {
+                throw new \Exception('Invalid file');
+            }
+        }
+        exec(sprintf("%s -copy none %s > %s", $this->moz_jpeg, '/tmp/mozjpeg_tmp.jpg', $dest_file));
+        unlink('/tmp/mozjpeg_tmp.jpg');
+        $dest_size = filesize($dest_file);
+        $output->writeln(
+            sprintf(
+                'Optimizando imagen "%s"%s: %s --> %s',
+                $source,
+                ($quality) ? ' (' . (int)$quality . '%)' : '',
+                $this->humanFilesize($source_size),
+                $this->humanFilesize($dest_size)
+            )
+        );
+        $this->optimized_images[] = $source_size - $dest_size;
     }
 
     /**
